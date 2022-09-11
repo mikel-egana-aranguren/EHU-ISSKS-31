bbd9d2a4d756204a4d36a8533a614e07b5a060ed
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-09-11 17:13:01 +0200

    Informazioaren Osotasuna ziurtatu

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -4,5 +4,5 @@ for dir in */;
 do 
     proper_dir=${dir%/}
     echo $PWD/$proper_dir
-    shower pdf --cwd $PWD/$proper_dir
+    shower pdf --cwd $PWD/$proper_dir -o $PWD/$proper_dir/index.pdf
 done
