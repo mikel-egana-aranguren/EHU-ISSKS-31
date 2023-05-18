48d421952ebf3ea8482c81c4b7d13ab110cccd87
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2023-05-18 11:42:13 +0200

    generate

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
-
-for dir in */;
-do 
-    proper_dir=${dir%/}
-    m0=$(cat $PWD/$proper_dir/index.html.md5sum)
-    m1=$(md5sum $PWD/$proper_dir/index.html  | cut -d' ' -f1)
-    echo $m1 > $PWD/$proper_dir/index.html.md5sum
-    if [ "$m0" != "$m1" ]; then
-        shower pdf --cwd $PWD/$proper_dir -o $PWD/$proper_dir/index.pdf
-    fi
-done
+/home/mikel/EHU-LSI/Docencia/2023-2024/SGSSI/EHU-SGSSI-01/generate_pdf.sh
